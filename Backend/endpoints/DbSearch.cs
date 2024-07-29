4f75134789608cb346cdae49a20b147843dbd23b
Alix <Alix@LAPTOP-95BQ3G03>
2024-07-29 18:43:29 -0500

    PUT Request created, Needs Validationgit add .!

--- Backend/endpoints/DbSearch.cs
+++ Backend/endpoints/DbSearch.cs
@@ -14,6 +14,8 @@ public static class DbSearch
     {
         var group = app.MapGroup("DbSearch");
 
+        // GET REQUESTS
+
         group.MapGet("/", ([FromServices] MySqlConnection connection) =>
         {
             var users = connection.Query<UserDtos>("SELECT * FROM user");
@@ -42,10 +44,12 @@ public static class DbSearch
                 return Results.NotFound();
         });
 
-        group.MapPost("/", (UserDtos newUser, [FromServices] MySqlConnection connection) =>
+        // POST REQUESTS
+
+        group.MapPost("/", async (UserDtos newUser, [FromServices] MySqlConnection connection) =>
         {
             if(newUser.FirstName != "" || newUser.FirstName != null) {
-                connection.ExecuteAsync(@"
+                await connection.ExecuteAsync(@"
                     INSERT INTO user (FirstName, LastName, Username, Password)
                         VALUES (@FirstName, @LastName, @Username, @Password);", newUser);
 
@@ -54,6 +58,25 @@ public static class DbSearch
             else {
                 return Results.BadRequest();
             }
+        });
+
+        // PUT REQUESTS
+
+        group.MapPut("/{Id}", async (int Id, UserDtos updateUsers, [FromServices] MySqlConnection connection) => {
+            await connection.ExecuteAsync(@"
+            UPDATE user SET 
+                `FirstName` = @FirstName, 
+                `LastName` = @LastName, 
+                `Username` = @Usern, 
+                `Password` = @Password
+            WHERE Id = @Id",
+            new { FirstName = updateUsers.FirstName,
+                  LastName = updateUsers.LastName,
+                  Usern = updateUsers.UserName,
+                  Password = updateUsers.Password,
+                  Id = Id });
+
+            return Results.Ok(updateUsers);
         });
 
         return group;
